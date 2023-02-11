d29f93d78bbf0237d24304fa847215d6a0831992
Elena9802 <119110019+Elena9802@users.noreply.github.com>
2023-02-11 15:54:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.screenmanager import MDScreenManager
+from kivy.clock import Clock
+from kivymd.uix.label import MDLabel
+from  kivy.properties import BooleanProperty
 
 P1 = 0
 P2 = 0
 P3 = 0
+age = 0
+
+class CountDown(MDLabel):
+    is_done = BooleanProperty(False)
+
+    def start(self):
+        Clock.schedule_interval(self.tick, 1)
+
+    def tick(self, tm):
+        print(tm)
+        t = int(self.text)
+        if t > 0:
+            self.text = str(t - 1)
+        else:
+            self.is_done = True 
+            self.text = 'Готово'
+            return False
+
+class Screen0(Screen):
+    def move_next(self):
+        global age
+        age_field = self.ids['age_field']
+        try:
+            P1 = int(self.ids["p1_field"].text)
+            if age < 7:
+                age_field.error = True
+                age_field.helper_text = 'Минимальный возраст 7 лет'
+            else:
+                self.parent.transition.direction = "left"
+                self.parent.current = "Screen1"
+        except:
+            age_field.error = True
+            age_field.helper_text = 'Введите целое число'
 
 class Screen1(Screen):
     """ Экран с инструкцией """
     def move_next(self):
         global P1
-        P1 = int(self.ids["p1_field"].text)
+        p1_field = self.ids['p1_field']
+        try:
+            P1 = int(self.ids["p1_field"].text)
+            if P1 <= 0:
+                p1_field.error = True
+                p1_field.helper_text = 'Введите число больше 0'
+            else:
+                self.parent.transition.direction = "left"
+                self.parent.current = "Screen2"
+        except:
+            p1_field.error = True
+            p1_field.helper_text = 'Введите целое число'
 
 class Screen2(Screen):
     """ Экран приседаний """
@@ -20,7 +67,15 @@ class Screen3(Screen):
     """ Экран измерения пульса """
     def move_next(self):
         global P2
-        P2 = int(self.ids["p2_field"].text)
+        p2_field = self.ids['p2_field']
+        try:
+            P2 = int(self.ids["p2_field"].text)
+        except:
+            p2_field.error = True
+            p2_field.helper_text = 'Введите целое число'
+
+
+
 
 class Screen4(Screen):
     """ Экран отдыха """
@@ -31,8 +86,24 @@ class Screen5(Screen):
     def move_next(self):
         global P3
         P3 = int(self.ids["p3_field"].text)
-        screen6 = self.parent.get_screen("Screen6")
-        screen6.ids["result_label"].text = f"P1={P1} P2={P2} P3={P3}"
+        screen6 = self.parent.get_screen('Screen6')
+        kf = (4 * (P1 + P2 + P3) - 200 ) / 10
+        n = (min(15, age) - 7) // 2
+        x = 21 - 1.5 * n
+        if kf >= x:
+            res = 'низкий'
+        elif kf >= x - 4 and kf < x:
+            res = 'Удолетворительный'
+        elif kf >= x - 9 and kf < x - F:
+            res = 'Средний'
+        elif kf >= x - 14.6 and kf < x - 9:
+            res = 'Выше мреднего'
+        else:
+            res = 'Высокий'
+        screen6.aids['results_label'].text = f'Ваш результат : {res} Ваш коэф. '
+        
+
+
 
 class Screen6(Screen):
     """ Экран результата """
